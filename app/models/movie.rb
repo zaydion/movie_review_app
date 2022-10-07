5f47e0aab254ad945dda8c8697f39c664f8a93f2
Jose Cartagena <jcartagena93@gmail.com>
2022-10-07 14:35:43 -0400

    adds delete feature + some Movie class methods

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,20 @@
 class Movie < ApplicationRecord
+  def self.hits
+    where("total_gross >= ?", 300_000_000).order(total_gross: :desc)
+  end
+
+  def self.flops
+    where("total_gross < ?", 225_000_000).order(:total_gross)
+  end
+
+  def self.recently_added
+    Movie.order(created_at: :desc).limit(3)
+  end
+
+  def self.released
+    where("released_on <= ?", Time.current).order(released_on: :desc)
+  end
+
   def flop?
     total_gross.blank? || total_gross < 225_000_000
   end
