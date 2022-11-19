ce41c7f27f643c84bd1c58a23557ffd84cf4c99e
Jose Cartagena <jcartagena93@gmail.com>
2022-11-19 13:40:24 -0500

    generates favorites model to join users and movies

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
   has_many :reviews, dependent: :destroy
+  has_many :favorites, dependent: :destroy
 
   validates :name, presence: true
   validates :email, presence: true,
