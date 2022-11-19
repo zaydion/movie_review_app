1f4ff23bcfa1a30ee279d3a7b175fb5ae717f827
Jose Cartagena <jcartagena93@gmail.com>
2022-11-19 15:12:45 -0500

    adds through association

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   has_secure_password
   has_many :reviews, dependent: :destroy
   has_many :favorites, dependent: :destroy
+  has_many :favorite_movies, through: :favorites, source: :movie
 
   validates :name, presence: true
   validates :email, presence: true,
