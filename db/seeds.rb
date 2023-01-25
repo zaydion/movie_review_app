0feeaf53eb74e4b28c7ce93c81fa4be8b473de14
Jose Cartagena <jcartagena93@gmail.com>
2023-01-25 10:43:21 -0500

    updates seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,7 +11,6 @@ Movie.create!([
     total_gross: 1_223_641_414,
     director: "Anthony Russo",
     duration: "181 min",
-    image_file_name: "avengers-end-game.png",
   },
   {
     title: "Captain Marvel",
@@ -23,7 +22,6 @@ Movie.create!([
     total_gross: 1_110_662_849,
     director: "Anna Boden",
     duration: "124 min",
-    image_file_name: "captain-marvel.png",
   },
   {
     title: "Black Panther",
@@ -35,7 +33,6 @@ Movie.create!([
     total_gross: 1_346_913_161,
     director: "Ryan Coogler",
     duration: "134 min",
-    image_file_name: "black-panther.png",
   },
   {
     title: "Avengers: Infinity War",
@@ -47,7 +44,6 @@ Movie.create!([
     total_gross: 2_048_359_754,
     director: "Anthony Russo",
     duration: "149 min",
-    image_file_name: "avengers-infinity-war.png",
   },
   {
     title: "Green Lantern",
@@ -59,7 +55,6 @@ Movie.create!([
     total_gross: 219_851_172,
     director: "Martin Campbell",
     duration: "114 min",
-    image_file_name: "green-lantern.png",
   },
   {
     title: "Fantastic Four",
@@ -71,7 +66,6 @@ Movie.create!([
     total_gross: 168_257_860,
     director: "Josh Trank",
     duration: "100 min",
-    image_file_name: "fantastic-four.png",
   },
   {
     title: "Iron Man",
@@ -85,7 +79,6 @@ Movie.create!([
     total_gross: 585_366_247,
     director: "Jon Favreau",
     duration: "126 min",
-    image_file_name: "ironman.png",
   },
   {
     title: "Superman",
@@ -99,7 +92,6 @@ Movie.create!([
     total_gross: 300_451_603,
     director: "Richard Donner",
     duration: "143 min",
-    image_file_name: "superman.png",
   },
   {
     title: "Spider-Man",
@@ -114,7 +106,6 @@ Movie.create!([
     total_gross: 825_025_036,
     director: "Sam Raimi",
     duration: "121 min",
-    image_file_name: "spiderman.png",
   },
   {
     title: "Batman",
@@ -127,7 +118,6 @@ Movie.create!([
     total_gross: 411_348_924,
     director: "Tim Burton",
     duration: "126 min",
-    image_file_name: "batman.png",
   },
   {
     title: "Catwoman",
@@ -139,7 +129,6 @@ Movie.create!([
     total_gross: 82_102_379,
     director: "Jean-Christophe 'Pitof' Comar",
     duration: "101 min",
-    image_file_name: "catwoman.png",
   },
   {
     title: "Wonder Woman",
@@ -151,7 +140,6 @@ Movie.create!([
     total_gross: 821_847_012,
     director: "Patty Jenkins",
     duration: "141 min",
-    image_file_name: "wonder-woman.png",
   },
 ])
 
